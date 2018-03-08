8cfa4d2d543f2cc4d96b417a0b68c1a607bdaaf6
Krzysztof <musashimiyamoto@interia.eu>
2018-03-08 02:19:17 +0100

    WTF

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Krzysiek2017:1234@ds259258.mlab.com:59258/nodeappdatabase', {
+mongoose.connect('mongodb://Krzysiek2017:1234@ds259258.mlab.com:59258/mod_9zad_1', {
     useMongoClient: true
 });
 
