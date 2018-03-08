ce8b3d19a35e36cf49d1df417291f0899e6e4e98
Krzysztof <musashimiyamoto@interia.eu>
2018-03-08 19:05:27 +0100

    zmiana

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Krzysiek2017:hopcias2017@ds261088.mlab.com:61088/nodeappdatabase', {
+mongoose.connect('mongodb://Krzysiek2017:Hopcias2017&@ds261088.mlab.com:61088/nodeappdatabase', {
     useMongoClient: true
 });
 
